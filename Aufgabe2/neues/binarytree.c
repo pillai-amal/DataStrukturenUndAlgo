6c1580655030fb066c608f5181d79922a956d565
pillai-amal <mail.vortextv@gmail.com>
2023-10-12 19:40:07 +0300

    bis search geht ohne Fehler

--- Aufgabe2/neues/binarytree.c
+++ Aufgabe2/neues/binarytree.c
@@ -1,36 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Data structure
-//----------------------------------------------------------------
+// Data structure
 struct Bintree {
     int data;
     struct Bintree* left;
     struct Bintree* right;
 };
-//----------------------------------------------------------------
 
-//Function Declarations
-//----------------------------------------------------------------
+// Function Declarations
 struct Bintree* insert(struct Bintree* root, int data);
-void search(struct Bintree* root, int data);
+void search(struct Bintree* root, int s_data);
 void printTree(struct Bintree* root);
 void freeTree(struct Bintree* root);
-void displayMenu();
-//----------------------------------------------------------------
+void displayMenu(struct Bintree** root);
 
-//Main Function
-//----------------------------------------------------------------
+// Main Function
 int main() {
-
     struct Bintree* root = NULL;
-    displayMenu();
+    displayMenu(&root);
     return 0;
 }
-//----------------------------------------------------------------
 
-//Function Definitions
-//----------------------------------------------------------------
+// Function Definitions
 struct Bintree* insert(struct Bintree* root, int data) {
     if (root == NULL) {
         struct Bintree* newNode = (struct Bintree*)malloc(sizeof(struct Bintree));
@@ -49,14 +41,18 @@ struct Bintree* insert(struct Bintree* root, int data) {
 }
 
 void search(struct Bintree* root, int s_data) {
-    if(root->data == s_data) {
-        printf("%d is found in the tree", s_data);
-    } else if(root->data > s_data) { 
-        search(root->right, s_data);
-    } else {
-        search(root->left, s_data);
+    if (root == NULL) {
+        printf("%d is NOT found in the tree\n", s_data);
+        return;
     }
-    printf("%d is NOT found in the tree", s_data);
+
+    if (root->data == s_data) {
+        printf("%d is found in the tree\n", s_data);
+    } else if (s_data < root->data) {
+        search(root->left, s_data);
+    } else {
+        search(root->right, s_data);
+    }
 }
 
 void printTree(struct Bintree* root) {
@@ -75,10 +71,16 @@ void freeTree(struct Bintree* root) {
     }
 }
 
-void displayMenu() {
+void displayMenu(struct Bintree** root) {
+    int choice, data, searchValue;
+
     do {
-        int choice, data;
-        displayMenu();
+        printf("\nASSIGNMENT II - TREE\n");
+        printf("\n1. Insert data\n");
+        printf("2. Print Tree (Inorder Traversal)\n");
+        printf("3. Search for value\n");
+        printf("4. Free Tree\n");
+        printf("5. Exit\n");
         printf("Enter your choice: ");
         scanf("%d", &choice);
 
@@ -87,26 +89,32 @@ void displayMenu() {
                 do {
                     printf("Enter data to insert: ");
                     scanf("%d", &data);
-                    root = insert(root, data);
+                    *root = insert(*root, data);
                     printf("Do you want to insert more elements? (1 for Yes, 0 for No): ");
                     scanf("%d", &choice);
                 } while (choice == 1);
                 break;
             case 2:
                 printf("Binary Tree (Inorder Traversal):\n");
-                printTree(root);
+                printTree(*root);
                 printf("\n");
                 break;
             case 3:
-                printf("Freeing the binary tree...\n");
-                freeTree(root);
-                printf("Exiting program.\n");
+                printf("Enter the value to search for: ");
+                scanf("%d", &searchValue);
+                search(*root, searchValue);
                 break;
             case 4:
+                printf("Freeing the binary tree...\n");
+                freeTree(*root);
+                *root = NULL;
+                printf("Tree freed.\n");
+                break;
+            case 5:
                 printf("Exiting program.\n");
                 break;
             default:
                 printf("Invalid choice. Please try again.\n");
         }
-    } while (choice != 4);
+    } while (choice != 5);
 }
